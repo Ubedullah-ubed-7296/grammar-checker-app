0f425a650ee0ca6921910b7c1c15c11678f5596a
Ubedullah <138193579+Ubedullah-ubed-7296@users.noreply.github.com>
2025-04-14 21:35:31 +0530

    grammar_checker_app.py

--- grammar_checker_app.py
+++ grammar_checker_app.py
@@ -4,46 +4,33 @@ from nltk import pos_tag, download
 from nltk.tokenize import word_tokenize
 from nltk.data import find
 
-# Set Streamlit page config FIRST
+# Set Streamlit page config
 st.set_page_config(page_title="Grammar Checker", layout="centered")
 
-# Function to download required NLTK data
-import nltk
-from nltk.data import find
-import streamlit as st
-
+# Download required NLTK data
 def download_nltk_data():
-    # Download 'punkt' tokenizer
     try:
         find('tokenizers/punkt')
     except LookupError:
-        try:
-            st.info("Downloading required tokenizer: punkt")
-            nltk.download('punkt', quiet=True, raise_on_error=False)
-        except:
-            st.error("Could not download 'punkt' tokenizer.")
+        st.info("Downloading: punkt tokenizer")
+        nltk.download('punkt')
 
-    # Download 'averaged_perceptron_tagger'
     try:
         find('taggers/averaged_perceptron_tagger')
     except LookupError:
-        try:
-            st.info("Downloading required tagger: averaged_perceptron_tagger")
-            nltk.download('averaged_perceptron_tagger', quiet=True, raise_on_error=False)
-        except:
-            st.error("Could not download 'averaged_perceptron_tagger'.")
+        st.info("Downloading: averaged_perceptron_tagger")
+        nltk.download('averaged_perceptron_tagger')
 
-# Download necessary NLTK data
 download_nltk_data()
 
-# Grammar rules: (POS tag pairs → status)
+# Grammar rules
 grammar_rules = [
     (('DT', 'JJ', 'NN'), "OK"),
     (('DT', 'NN'), "OK"),
     (('JJ', 'NN'), "OK"),
-    (('NN', 'VB'), "Error"),       
-    (('VB', 'VB'), "Check"),       
-    (('RB', 'RB'), "Check")        
+    (('NN', 'VB'), "Error"),
+    (('VB', 'VB'), "Check"),
+    (('RB', 'RB'), "Check")
 ]
 
 # Grammar checking function
@@ -55,9 +42,8 @@ def check_grammar(sentence):
     for i in range(len(tagged) - 1):
         pair = (tagged[i][1], tagged[i+1][1])
         for rule in grammar_rules:
-            if pair == rule[0][:2]:
-                if rule[1] != "OK":
-                    issues.append((tagged[i][0], tagged[i+1][0], rule[1]))
+            if pair == rule[0][:2] and rule[1] != "OK":
+                issues.append((tagged[i][0], tagged[i+1][0], rule[1]))
     return tagged, issues
 
 # Streamlit UI
