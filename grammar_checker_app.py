68d6e5c40db50a665871252580a499e99c03ecb5
Ubedullah <138193579+Ubedullah-ubed-7296@users.noreply.github.com>
2025-04-14 22:41:22 +0530

    grammar_checker_app.py

--- grammar_checker_app.py
+++ grammar_checker_app.py
@@ -1,52 +1,40 @@
 import streamlit as st
 import nltk
-from nltk import pos_tag, download
+from nltk import pos_tag
 from nltk.tokenize import word_tokenize
-from nltk.data import find
 
-# Set Streamlit page config
-st.set_page_config(page_title="Grammar Checker", layout="centered")
+# Download required data
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-# Download required NLTK data
-def download_nltk_data():
-    try:
-        find('tokenizers/punkt')
-    except LookupError:
-        st.info("Downloading: punkt tokenizer")
-        nltk.download('punkt')
-
-    try:
-        find('taggers/averaged_perceptron_tagger')
-    except LookupError:
-        st.info("Downloading: averaged_perceptron_tagger")
-        nltk.download('averaged_perceptron_tagger')
-
-download_nltk_data()
-
-# Grammar rules
+# Define grammar rules
 grammar_rules = [
     (('DT', 'JJ', 'NN'), "OK"),
     (('DT', 'NN'), "OK"),
     (('JJ', 'NN'), "OK"),
-    (('NN', 'VB'), "Error"),
-    (('VB', 'VB'), "Check"),
-    (('RB', 'RB'), "Check")
+    (('NN', 'VB'), "Error"),       
+    (('VB', 'VB'), "Check"),       
+    (('RB', 'RB'), "Check")        
 ]
 
-# Grammar checking function
 def check_grammar(sentence):
     tokens = word_tokenize(sentence)
     tagged = pos_tag(tokens)
     issues = []
 
+    # Check consecutive pairs
     for i in range(len(tagged) - 1):
         pair = (tagged[i][1], tagged[i+1][1])
         for rule in grammar_rules:
-            if pair == rule[0][:2] and rule[1] != "OK":
-                issues.append((tagged[i][0], tagged[i+1][0], rule[1]))
+            if pair == rule[0]:
+                if rule[1] != "OK":
+                    issues.append((tagged[i][0], tagged[i+1][0], rule[1]))
+
     return tagged, issues
 
 # Streamlit UI
+st.set_page_config(page_title="Grammar Checker", layout="centered")
+
 st.title("📝 Simple Grammar Checker")
 sentence = st.text_area("Enter a sentence to check:", height=150)
 
